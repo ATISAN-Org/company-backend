b266debc20c97102068355165efa4a35a13e9aee
mahinhasan <mahinhasanaiub@gmail.com>
2025-07-13 13:37:02 +0600

    Updated

--- atisan/atisan/settings.py
+++ atisan/atisan/settings.py
@@ -56,8 +56,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
+        'rest_framework.permissions.AllowAny',
+    )
 }
 
 ROOT_URLCONF = 'atisan.urls'
